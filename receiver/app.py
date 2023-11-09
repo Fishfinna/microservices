6527e7fa78e7fb4ad6a94543e082b6a8760a29ca
Shanti <fishfinna12@gmail.com>
2023-11-09 11:06:23 -0800

    kafka

--- receiver/app.py
+++ receiver/app.py
@@ -23,15 +23,32 @@ with open("log_conf.yml", "r") as f:
     logger = logging.getLogger("basicLogger")
 
 
+def kafka_connect():
+    max_retries = int(app_config["max_retries"])
+    current_retry_count = 0
+    while current_retry_count < max_retries:
+        try:
+            logger.info(
+                f"Attempting connection to Kafka (attempt {current_retry_count} of {max_retries})"
+            )
+            client = KafkaClient(
+                hosts=f'{events_config.get("hostname")}:{events_config.get("port")}'
+            )
+            logger.info(f"Successfully connected to kafka")
+            break
+        except Exception as e:
+            logger.error(f"attempt {current_retry_count} failed to connect to kafka")
+            time.sleep(app_config["sleep_time"])
+            current_retry_count += 1
+        if current_retry_count == max_retries:
+            logger.error("Failed to connect to kafka")
+
+
 def report_asteroid_direction(body):
     event_name = "save-direction"
     trace_id = str(uuid.uuid4())
     body["trace_id"] = trace_id
     logger.info(f"Received {event_name} request with a trace id of {trace_id}) ")
-
-    client = KafkaClient(
-        hosts=f'{events_config.get("hostname")}:{events_config.get("port")}'
-    )
     topic = client.topics[str.encode(events_config["topic"])]
     producer = topic.get_sync_producer()
     msg = {
@@ -52,10 +69,6 @@ def report_asteroid_scale(body):
     trace_id = str(uuid.uuid4())
     body["trace_id"] = trace_id
     logger.info(f"Received {event_name} request with a trace id of {trace_id}) ")
-
-    client = KafkaClient(
-        hosts=f'{events_config.get("hostname")}:{events_config.get("port")}'
-    )
     topic = client.topics[str.encode(events_config["topic"])]
     producer = topic.get_sync_producer()
     msg = {
@@ -75,4 +88,5 @@ app = connexion.FlaskApp(__name__, specification_dir="")
 app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
+    kafka_connect()
     app.run(port=8080)
