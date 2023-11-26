9e271ce8282ee489ed14829db64f2bd9399c597e
Shanti <fishfinna12@gmail.com>
2023-11-25 21:16:40 -0800

    made some changes for the ci

---
+++ shared_pipeline/vars/microservice_pipeline.groovy
@@ -0,0 +1,40 @@
+def call(dockerRepoName, imageName) {
+    pipeline {
+        agent any
+        parameters {
+            booleanParam(defaultValue: false, description: 'Deploy the App', name: 'DEPLOY')
+        }
+        stages {
+            stage('Lint') {
+                steps {
+                    sh "pylint --fail-under=5.0 *.py"
+                }
+            }
+            stage('Security Check') {
+                script {
+                    sh 'pip install safety --break-system-packages'
+                    sh 'safety check -r requirements.txt --full-report'
+                }
+            }
+            stage('Package') {
+                steps {
+                    withCredentials([string(credentialsId: 'DockerHub', variable: 'TOKEN')]) {
+                        sh "docker login -u 'fishfinna' -p '$TOKEN' docker.io"
+                        sh "docker build -t ${dockerRepoName}:latest --tag fishfinna/${dockerRepoName}:${imageName} ."
+                        sh "docker push fishfinna/${dockerRepoName}:${imageName}"
+                    } 
+                }
+            }
+            stage("Deploy") {
+                when {
+                    expression { params.DEPLOY }
+                }
+                steps {
+                    sshagent(credentials: ['shanti-kafka-ssh']) {
+                        sh "ssh azureuser@20.151.78.202 'cd ~/microservices/deployment && docker-compose pull && docker-compose up -d'"
+                    }
+                }
+            }
+        }
+    }
+}
